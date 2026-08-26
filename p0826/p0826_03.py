332d3ed13d44ca5f6dddf61fcf6e1e875af9ae38
namunege38@gmail.com <namunege38@gmail.com>
2026-08-26 15:45:30 +0900

    산술연선

---
+++ p0826/p0826_03.py
@@ -0,0 +1,50 @@
+# print : 출력
+# nput : 입력
+# num = input("숫자를 입력하세요.")
+# print("입력숫자 : {}".format(num))
+
+# a = 10
+# b = 3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b) #10*10*10 / 10dml 3승
+
+
+# num1 = 100
+# num2 = 100
+# num3 = 100
+# orint(num1,num2,num3)
+
+
+# # 한줄에 여러 변수에 여러개 값을 넣는 것은 불가
+# # a1=1,a2=2 에러
+# num4=num5=num6=1
+# print(num4,num5,num6)
+
+# a1=1,a2="안녕"
+# a2=2
+# print(a1=a2)
+
+# no1 = 100 # 변수선언과 동시에 값전달
+# print()
+
+
+# input으로 받은 모든 것은 문자열타입
+# a = int(input("1번째 숫자를 입력하세요.")) # str타입을 int타입으로 변경
+# b = int(input("2번째 숫자를 입력하세요.")) 
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b) #10*10*10 / 10dml 3승
+
+# 아이디,패스워드를 입력받아 출력하시오.
+# 아이디:aaa, 패스워드:1111
+
+id = input("아이디를 입력하세요")
+pw = input("패스워드를 입력하세요")
+print("아이디확인 : {}".format("aaa"==id)) #True,False
+print("패스워드확인 : {}".format("1111"==pw)) #True,False
+print("아이디:{},패스워드:{}".format{id,pw})
