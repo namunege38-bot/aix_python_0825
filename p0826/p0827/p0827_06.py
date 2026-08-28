a714100d458e2f40513e773cb4985391fd66b4e0
namunege38@gmail.com <namunege38@gmail.com>
2026-08-28 16:04:03 +0900

    조건문,반복문

--- p0826/p0827/p0827_06.py
+++ p0826/p0827/p0827_06.py
@@ -1,32 +1,170 @@
-import random
-random_no = random.randint(-10,10)
-print("랜덤숫자 : ",rnadom_no)
+score = 65
+# score 60점 이상이면 합격, 불합격
+if score>+60: print("합격")
+else: print("불합격")
 
-# 양수,음수인지 출력하시오.
+# if문 축약
+result = "합격" if score>=60 else"불합격" # 명령어가 한줄때만 축약 가능
 
-if score>0:
-    print("양수")
-elif score<1:
-    print("음수")
 
-# 조건문을 여러개
-# score = 80
+
+# # 날짜함수를 사용하려면
+# import datetime
+
+# now = datetime.datetime.now()
+
+# # 해당 월에 따라 봄,여름,가을,겨울이라고 출력하시오.
+# # 겨울 12,2,1 봄 3,4,5 여름 6,7,8 겨울 9,10,11
+# # 비교문을 사용해서
+# # 해당월 계절을 출력하시오.
+# # now.momth
+
+# 겨울 = 1,2,3
+# 봄 = 3,4,5
+# 가을 = 6,7,8
+# 겨울 = 9,10,11
+
+# month = now.month
+# month = int(input("월을 입력하세요."))
+# if month==12 or 1<=month<=2:
+#     print("겨울입니다.")
+# elif 8>=month>=6:
+#     print("여름입니다.")
+# elif 5>=month>=3:
+#     print("봄입니다.")
+# else:
+#     print("가을입니다.")
+
+# if : 조건문
+# if
+# if - else
+# if elif else
+# if elif elif else
+
+# if 조건문
+#    들여쓰기가 되어야 함.
+# else:
+#   들여쓰기 되어야 함.
+
+# if 10>5:
+#     pass    # 출력이나 기타 프로그램이 없을시 pass
+#    # 빈 공백이면 에러남.
+# print("프로그램")
+
+# if 10>5: pass
+# if 10>5: print("참") # if 한줄가능
+# if 10>5:
+#     print("참")
+
+# if 10>5:   # 명령어가 2줄이상이면 다음줄에 넣어야 함
+#     print("참")
+#     print("좋아요")    
+# import random
+# 랜덤점수를 생성해서
+# 90점 이상 A, 80점 이상 B, 7점 이상 C, 그외에는 F
+# 랜덤점수 출력하시오.
+
+# import random
+# score = int(input("점수를 입력하세요:"))
 # if score>=90:
-#       print("A")
+#     print("A")
 # elif score>=80:
-#       print("B")
-# elif score>=70:
-#       print("C")
+#     print("B")
 # elif score>=60:
-#       print("D")
+#     print("C")
 # else:
-#       print("F")
+#     print("F")
 
-# a = 105
-# if a>50:
-#     if a<100:
-#         print("50보다 크고, 100보다 작은수")
+
+
+# import random
+# # 랜덤점수를 생성해서
+# # 90점이상 A, 80점이상 B,70-C,60-D,F 출력하시오.
+# # 90-92점 A-, 93-97 A, 98 A+
+# # 80-82점 B-, 83-87 B, 88 B+
+# # 70-72점 C-, 73-77 C, 78 C+
+# # 랜덤점수 출력하시오.
+
+# num = random.randint(0,100)
+# if num>=90:
+#     if num>=99:
+#         print("A+")
+#     elif num>=93:
+#         print("A")
 #     else:
-#         print("50보다 크고, 100보다 큰수")
+#         print("A-")
+# elif num>=80:
+#     if num>=89:
+#         print("B+")
+#     elif num>=83:
+#         print("B")
+#     else:
+#         print("B-")
+# elif num>=70:
+#     if num>=79:
+#         print("C+")
+#     elif num>=73:
+#         print("C")
+#     else:
+#         print("C-")
+# elif num>=60:
+#     if num>69:
+#         print("D+")
+#     elif num>=63:
+#         print("D")
+#     else:
+#         print("D-")
 # else:
-    # print("50보다 작은수")
+#     print("F")
+
+# import random
+# # 0-100점 랜덤숫자 생성
+# # 60점 이상 합격
+# # 50점-59점까지 재시험
+# # 0-49점까지 불합격
+
+# import random
+# score = random.randint(0,100)
+# if score>=60:
+#      print("합격입니다")
+# elif 59>=score>=50: # score>=50: 도 가능
+#      print("재시험입니다")
+# else:
+#      print("불합격입니다")
+# print("랜덤점수 : ",score)
+     
+
+# import random
+# random_no = random.randint(-2,2)
+# print("랜덤숫자 : ",random_no)
+
+# # 양수,음수인지 출력하시오.
+
+# if random_no>0:
+#     print("양수")
+# elif random_no == 0:
+#     print("0입니다.")
+# else:
+#     print("음수")
+
+# # 조건문을 여러개
+# # score = 80
+# # if score>=90:
+# #       print("A")
+# # elif score>=80:
+# #       print("B")
+# # elif score>=70:
+# #       print("C")
+# # elif score>=60:
+# #       print("D")
+# # else:
+# #       print("F")
+
+# # a = 105
+# # if a>50:
+# #     if a<100:
+# #         print("50보다 크고, 100보다 작은수")
+# #     else:
+# #         print("50보다 크고, 100보다 큰수")
+# # else:
+#     # print("50보다 작은수")
---
+++ p0828/p0828_08.py
@@ -0,0 +1,123 @@
+a = [1,2,3,4,5]
+print(a)
+a{2}=30
+print(a)
+a.append(200)
+print(a)
+
+
+
+
+# import datetime
+# import random
+# #1-45까지 랜덤5개를 가져와서
+# # 입력한 숫자가 있으면 당첨, 없으면 꽝
+# # 3개만 우선
+# # 5개
+
+# lotto = random.sample(range(1,45),5)
+# print(lotto)
+# iarr=[]
+# # 반복문
+# for i in range(5):
+#     iarr.append(int(input("숫자입력 : ")))
+# for i in range(5:)
+# input = int(input("숫자입력 : "))
+
+# if iar
+# else:
+#  print("꽝")
+# if iarr in lotto:
+#  print("당첨")
+# else:
+#  print("꽝")
+# if iarr in lotto:
+#  print("당첨")
+# else:
+#  print("꽝")
+#  if iarr in lotto:
+#  print("당첨")
+# else:
+#  print("꽝")
+# if iarr in lotto:
+#  print("당첨")
+# else:
+#  print("꽝")
+# lotto1 = random.sample(range(1,45),5)
+# print(lotto1)
+# lotto2 = random.sample(range(1,45),5)
+# print(lotto2)
+# lotto3 = random.sample(range(1,45),5)
+# print(lotto3)
+# lotto4 = random.sample(range(1,45),5)
+# print(lotto4)
+# lotto5 = random.sample(range(1,45),5)
+# print(lotto5)
+
+# # arr = random.sample(range(1,46),5) # 1-45까지 중복없이 5개를 가져옴.
+# # print(arr)
+# # arr2 = random.sample([1,2,3],2)
+# # print(arr2)
+
+# # arr3 = ({1,2,3,4,5}) # 리스트 전체를 랜덤으로 섞어줌
+# # random.shuffle(arr3)
+# # print(arr3)
+
+# # arr4 = [1,2,3,4,5]
+# # random.choices(arr4,k=3) # 리스트 해당개수만큼 가져옴. 중복가능
+# # print(arr5)
+# # 비교해서 있으면 당첨, 없으면 꽝
+
+
+# # # 날짜함수,랜덤함수
+# # import datetime
+# # import random
+
+# # # # 
+# # # now = datetime.datetime.now()
+# # # #year,month,day,hour,second
+# # # print(now)
+# # # print(now.year)
+# # # print(now.month)
+
+# # # # random
+# # # r_num = random.randint(1,12)
+# # # # 3,4,5 봄 6,7,8 여름 9,10,11 가을, 12,1,2 겨울
+
+# # # if  3<=r_num<=5:
+# # #  print("봄")
+# # # elif 6<=r_num<=8:
+# # #  print("여름")
+# # # elif 9<=r_num<=11: # elif 9<=r_num and r_num<=11
+# # #  print("가을")
+# # # else:
+# # #  print("겨울")
+
+# #  # now.month
+# #  # 01,02월
+
+# #  # 랜덤 5개 출력
+# # # randint=랜덤1개, sample=랜덤 여러개(중복불가)
+# # # shuffle = 전체 섞음, choices = 랜덤여러개(중복가능)
+# # a = random.randint(1,45) #랜덤 1개
+# # arr = random.sample(range(1,46),5) # 1-45까지 중복없이 5개를 가져옴.
+# # print(arr)
+# # arr2 = random.sample([1,2,3],2)
+# # print(arr2)
+
+# # arr3 = ({1,2,3,4,5}) # 리스트 전체를 랜덤으로 섞어줌
+# # random.shuffle(arr3)
+# # print(arr3)
+
+# # arr4 = [1,2,3,4,5]
+# # random.choices(arr4,k=3) # 리스트 해당개수만큼 가져옴. 중복가능
+# # print(arr5)
+
+# # # 리스트 생성방법
+# # # alist = [0,0,0,0,0]
+# # # print(alist)
+# # # alist2 = [0]*3
+# # # alist3 = list(range(1,5))
+# # # print(alist1)
+# # # print(alist2)
+# # # print(alist3)
